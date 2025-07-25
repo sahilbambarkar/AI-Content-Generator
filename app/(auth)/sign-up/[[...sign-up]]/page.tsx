d905724c40b38b8926016358c7b4a6dfc11371df
Sahil Bambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-07-25 14:22:20 +0530

    added custom bg

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,16 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
     return (
-    <div className="flex items-center justify-center h-screen bg-slate-200 ">
+    <div className="flex items-center justify-center h-screen">
+
+            <Image
+                src="/Background.jpg"
+                alt="logo"
+                layout="fill"
+                objectFit="cover"
+            />
             <SignUp />
     </div>
 
