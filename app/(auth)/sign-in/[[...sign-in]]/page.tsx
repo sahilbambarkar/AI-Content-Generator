fcb64c3a158059642494e1102883340f708529c0
Sahil Bambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-07-25 14:21:53 +0530

    added custom bg

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-    return( 
-    <div className="flex items-center justify-center h-screen bg-slate-200">
+    return (
+        <div
+            className="flex items-center justify-center h-screen"
+        >
+            <Image
+                src="/Background.jpg"
+                alt="logo"
+                layout="fill"
+                objectFit="cover"
+            />
             <SignIn />
-
-    </div>
-  
-);
+        </div>
+    );
 }
